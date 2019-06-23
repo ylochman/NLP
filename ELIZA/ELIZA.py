a3fbc1a7c8b3fd8d0072f5d959f285b20b46d30d
ylochman <lochman.y@gmail.com>
2019-06-23 19:38:52 +0000

    Update ELIZA

--- ELIZA/ELIZA.py
+++ ELIZA/ELIZA.py
@@ -42,10 +42,11 @@ psychobabble = [
         ]
     ),
     (
-        r'.* ?I dream about (.*)',
+        r'.* ?\bI dream about\b\|bI have a dream about\b (.*)',
         [
-            "Is {} a big dream for you?",
-            "What will you do after ?",
+            # "Why do you dream about it?",
+            # "Is it a big dream to you?",
+            # "What are the other dreams you have?",
             "What feelings do you have about {}?"
         ]
     ),
@@ -61,19 +62,64 @@ psychobabble = [
             "{}?",
             "Please tell me more about {}.",
         ]
-    ),    
+    ),
     (
-        r'.* ?(\bLOL|ha).*',
+        r'.* ?[Aa]m I (.*)?',
         [
-            "Why are you laughing?",
-            "Did I make you laugh?"
+        "Do you think you are {}?",
+        "Do you believe you are {}?",
+        "Would you want to be {}?",
+        "What would it mean if you were {}?",
         ]
     ),
     (
-        r'.{10,1000}',
+        r'.*?[Aa]re you (.*)?',
+        [
+        "Why are you interested in whether I am {} or not?",
+        "Would you prefer if I weren't {}?"
+        ]
+    ),
+    (
+        r'.*[Nn]o.*',
+        [
+            "Why not?",
+            "Are you saying 'No' just to be negative?"
+        ]
+    ),    
+    (
+        r'.*([Ll][Oo][Ll]|[Hh][Aa][Hh][Aa]|[Ll][Mm][Aa][Oo]|[Kk][Ee]+[Kk]).*',
+        [
+            "Why are you laughing?",
+            "Did I make you laugh?",
+            "What's fun?",
+            "Am I funny?"
+        ]
+    ),
+    (
+        r'[A-Za-z .!,\-0-9]{3,5000}\?',
+        [
+            "What do you think?",
+            "What are your thoughts about it?",
+            "What is your opinion?",
+            "Can you answer it?",
+            "That's hard to answer.",
+            "It depends.",
+        ]
+    ),   
+    (
+        r'[Qq]uit|[Ee]xit',
+        [
+            "Thank you for talking with me.",
+            "Good-bye.",
+            "Thank you, that will be $150. Have a good day!"
+        ]
+    ),   
+    (
+        r'.{10,5000}',
         [
             "Please tell me more about it.",
             "Tell me more about it.",
+            "Would you like to discuss it?",
             "That's interesting, please go on.",
             "What do you think about it?",
             "What do you feel about it?"
@@ -85,14 +131,6 @@ psychobabble = [
             "Please tell me more about it.",
             "Tell me more about it.",
             "Please go on.",
-        ]
-    ),   
-    (
-        r'quit',
-        [
-            "Thank you for talking with me.",
-            "Good-bye.",
-            "Thank you, that will be $150. Have a good day!"
         ]
     )
 ]
@@ -120,15 +158,17 @@ def main():
     "Hi. How are you?",
     "Hello. How are you feeling today?"
     ])
+    
     print("ELIZA: " + start)
 
     while True:
         statement = input("YOU: ")
         print("ELIZA: " + analyze(statement))
 
-        if statement == "quit":
+        if statement.lower() in ["quit", "exit"]:
             break
 
 
 if __name__ == "__main__":
+    # print(len(psychobabble))
     main()
